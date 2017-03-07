0facc02fe7789b3e669233a8383bb0bab51d830e
mattmartinez <mattmartinez816@gmail.com>
2017-03-07 13:08:47 -0800

    Configured back-end controllers

---
+++ controllers/users.js
@@ -0,0 +1,10 @@
+// REQUIREMENTS
+var express         = require('express');
+var router          = express.Router();
+var Book            = require('../models/book.js');
+var User            = require('../models/user.js');
+// var authHelpers  = require('../helpers/auth.js');
+
+
+// EXPORTS
+module.exports = router;
