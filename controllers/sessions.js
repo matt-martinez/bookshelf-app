87643b390af8c7687a3e9255828a7657610345d2
mattmartinez <mattmartinez816@gmail.com>
2017-03-07 15:41:56 -0800

    Sessions login and logout created

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -4,6 +4,20 @@ var router          = express.Router();
 var User            = require('../models/user.js');
 var authHelpers     = require('../helpers/auth.js');
 
+// Login
+router.post('/login', authHelpers.loginUser, function(req, res) {
+  console.log("User Logged In");
+  console.log(req.session.currentUser);
+  res.json({ status: 200, data: req.session.currentUser});
+});
+
+// Logout
+router.delete('/', function(req, res) {
+  console.log("User Logged Out");
+  req.session.destroy(function() {
+    res.json({ status: 204, message: "Session Destroyed" });
+  });
+});
 
 // EXPORTS
 module.exports = router;
