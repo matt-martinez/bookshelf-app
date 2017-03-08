4c5503d5dbe64bfcbba38a904b680cf35c41671c
mattmartinez <mattmartinez816@gmail.com>
2017-03-08 09:20:28 -0800

    Auth logout, created and successfully tested

--- public/scripts/controllers/authController.js
+++ public/scripts/controllers/authController.js
@@ -17,10 +17,21 @@ function AuthController($http, $state, $scope, $rootScope) {
         // console.log(response.data.data);
         $scope.$emit('loggedInUser', response.data.data);
         $rootScope.$emit('fetchData', response.data.data);
-        // $state.go('index');
+        $state.go('index');
       });
   }
 
+  function logout() {
+    console.log("Frontend Logout");
+    $http.delete('/sessions')
+      .then(function(response) {
+        // console.log(response);
+        $scope.$emit('loggedOutUser');
+        $state.go('index');
+      });
+  }
+
+  self.logout = logout;
   self.login = login;
   self.signup = signup;
 }
