3ddf54b02bbe568ea02fb233d48dc9fe17c55876
mattmartinez <mattmartinez816@gmail.com>
2017-03-08 07:57:44 -0800

    Home Controller built

--- public/scripts/controllers/homeController.js
+++ public/scripts/controllers/homeController.js
@@ -1,3 +1,11 @@
 function HomeController($scope, $http) {
-  
+  var self = this;
+
+  $scope.$on('loggedInUser', function(event, data) {
+    self.currentUser = data;
+  });
+
+  $scope.$on('loggedOutUser', function(event, data) {
+    self.currentUser = null;
+  });
 }
