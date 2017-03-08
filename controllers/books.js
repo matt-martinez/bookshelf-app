5fe41d195c37b5160e0a55640f823375f9db1780
mattmartinez <mattmartinez816@gmail.com>
2017-03-07 23:08:42 -0800

    Successfully tested backend book show and create routes with Postman

--- controllers/books.js
+++ controllers/books.js
@@ -19,8 +19,10 @@ router.get('/', function(req, res) {
 // Book Create
 router.post('/', function(req, res) {
   console.log("Backend Book Create")
-  User.findById(req.params.id).exec()
+  User.findById(req.session.currentUser._id).exec()
     .then(function(user) {
+      // console.log("looking for user");
+      // console.log(user);
       var newBook = new Book({
         title: req.body.title,
         author: req.body.author,
@@ -28,11 +30,20 @@ router.post('/', function(req, res) {
         pageCount: req.body.pageCount,
         status: req.body.status
       });
-
+      // console.log("After new book is entered");
+      // console.log(newBook);
       user.bookList.push(newBook);
-      user.save();
-      console.log(user);
+      // console.log("Checking to see if it pushed");
+      // console.log(user.bookList);
+      user.save()
+      // console.log("Logging New Book");
+      // console.log(newBook);
+      // console.log("Logging User Object");
+      // console.log(user);
       res.json({ user });
+    })
+    .catch(function(err) {
+      res.json(err)
     });
 });
 
