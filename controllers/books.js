3a7168a663693d8ace32dd3eb953a72ba4274768
mattmartinez <mattmartinez816@gmail.com>
2017-03-07 22:48:37 -0800

    Created backend book show and create routes

--- controllers/books.js
+++ controllers/books.js
@@ -5,6 +5,42 @@ var Book            = require('../models/book.js');
 var User            = require('../models/user.js');
 var authHelpers     = require('../helpers/auth.js');
 
+// Book Show
+router.get('/', function(req, res) {
+  console.log("Backend Book Show")
+  User.findById(req.session.currentUser._id)
+    .exec(function(err, user) {
+      if (err) { console.log(err); }
+      res.json({ currentUser: user });
+      console.log(user);
+    });
+});
+
+// Book Create
+router.post('/', function(req, res) {
+  console.log("Backend Book Create")
+  User.findById(req.params.id).exec()
+    .then(function(user) {
+      var newBook = new Book({
+        title: req.body.title,
+        author: req.body.author,
+        genre: req.body.genre,
+        pageCount: req.body.pageCount,
+        status: req.body.status
+      });
+
+      user.bookList.push(newBook);
+      user.save();
+      console.log(user);
+      res.json({ user });
+    });
+});
+
+
+// Book Update
+
+// Book Delete
+
 
 // EXPORTS
 module.exports = router;
