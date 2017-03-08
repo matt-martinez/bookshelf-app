1e6762f5fce3d7bfe1e1dec0124a14cc38c16b1f
mattmartinez <mattmartinez816@gmail.com>
2017-03-08 08:33:17 -0800

    Auth Controller signup

--- public/scripts/controllers/authController.js
+++ public/scripts/controllers/authController.js
@@ -1,3 +1,12 @@
 function AuthController($http, $state, $scope, $rootScope) {
-  
+  var self = this;
+
+  function signup(userPass) {
+    console.log("Frontend Sign Up");
+    $http.post('/users', userPass)
+      .then(function(response) {
+      // $state.go('index');
+    });
+  }
+
 }
