9aab5480f43f404ebaa081f48e7015e5400e5b17
mattmartinez <mattmartinez816@gmail.com>
2017-03-08 16:28:41 -0800

    Created and successfully tested editBook and cleaned up addbook

--- public/scripts/controllers/bookController.js
+++ public/scripts/controllers/bookController.js
@@ -9,7 +9,7 @@ function BookController($scope, $http, $state, $stateParams) {
 
   // User Books
   function getUserBooks(currentUser) {
-    console.log("FrontEnd Book Show")
+    console.log("FrontEnd Book Show");
     $http.get(`/users/${currentUser._id}/books`)
       .then(function(response) {
         console.log(response.data.currentUser.bookList);
@@ -20,28 +20,39 @@ function BookController($scope, $http, $state, $stateParams) {
   self.newBook = {title: '', author: '', genre: '', pageCount: '', status: ''};
   // Create
   function addBook(currentUser) {
-    console.log("FrontEnd Book Create")
-    self.newBook = {
-      title: self.newBook.title,
-      author: self.newBook.author,
-      genre: self.newBook.genre,
-      pageCount: self.newBook.pageCount,
-      status: self.newBook.status
-    }
+    console.log("FrontEnd Book Create");
     $http.post(`/users/${currentUser._id}/books`, self.newBook)
-    .then(function(response) {
-      console.log(response);
-      getUserBooks(currentUser);
-      self.newBook = {title: '', author: '', genre: '', pageCount: '', status: ''};
-    });
+      .then(function(response) {
+        console.log(response);
+        getUserBooks(currentUser);
+        self.newBook = {title: '', author: '', genre: '', pageCount: '', status: ''};
+      });
   }
 
   // Update
+  function editBook(currentUser, id) {
+    console.log("FrontEnd Book Edit");
+    console.log(currentUser.bookList);
+    console.log(id);
+    // console.log(currentUser.bookList.data._id);
+    self.updatedBook = {
+      title: self.updatedBook.title,
+      author: self.updatedBook.author,
+      genre: self.updatedBook.genre,
+      pageCount: self.updatedBook.pageCount,
+      status: self.updatedBook.status
+    }
+    $http.put(`/users/${currentUser._id}/books/${id}`, self.updatedBook)
+      .then(function(response) {
+        console.log(response);
+        getUserBooks(currentUser);
+      });
+  }
 
   // Delete
 
 
-
+  self.editBook = editBook;
   self.addBook = addBook;
   self.getUserBooks = getUserBooks;
 }
