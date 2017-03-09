96f86083b08070c189780d9b5d24b12d4ae38845
mattmartinez <mattmartinez816@gmail.com>
2017-03-08 16:35:20 -0800

    Cleaned up console.logs

--- public/scripts/controllers/bookController.js
+++ public/scripts/controllers/bookController.js
@@ -5,25 +5,24 @@ function BookController($scope, $http, $state, $stateParams) {
   $scope.$on('loginData', function(event, currentUser) {
     getUserBooks(currentUser)
   });
-  // Index
 
   // User Books
   function getUserBooks(currentUser) {
     console.log("FrontEnd Book Show");
     $http.get(`/users/${currentUser._id}/books`)
       .then(function(response) {
-        console.log(response.data.currentUser.bookList);
+        // console.log(response.data.currentUser.bookList);
         self.savedBooks = response.data.currentUser.bookList;
       });
   }
 
+// Create
   self.newBook = {title: '', author: '', genre: '', pageCount: '', status: ''};
-  // Create
   function addBook(currentUser) {
     console.log("FrontEnd Book Create");
     $http.post(`/users/${currentUser._id}/books`, self.newBook)
       .then(function(response) {
-        console.log(response);
+        // console.log(response);
         getUserBooks(currentUser);
         self.newBook = {title: '', author: '', genre: '', pageCount: '', status: ''};
       });
@@ -32,9 +31,8 @@ function BookController($scope, $http, $state, $stateParams) {
   // Update
   function editBook(currentUser, id) {
     console.log("FrontEnd Book Edit");
-    console.log(currentUser.bookList);
-    console.log(id);
-    // console.log(currentUser.bookList.data._id);
+    // console.log(currentUser.bookList);
+    // console.log(id);
     self.updatedBook = {
       title: self.updatedBook.title,
       author: self.updatedBook.author,
